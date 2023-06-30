8328b32aa795d1d539306a7be85d13f7fc872134
jitendra <gupta1jitu1@gmail.com>
2023-06-30 13:43:56 +0530

    secound commit

--- src/test/java/demoJenkins/UsingPOM/AppTest.java
+++ src/test/java/demoJenkins/UsingPOM/AppTest.java
@@ -10,6 +10,8 @@ public class AppTest {
   public void shouldAnswerWithTrue() {
 	  
 	  System.out.println("This is just an demo of project");
+	  System.out.println("This is just not an demo of project but seems maven-genkins-git integartion");
+
     assertTrue(true);
   }
 }
