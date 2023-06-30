0dc5e2bb617b784f9f1e67bc6c29b7b62a4af675
jitendra <gupta1jitu1@gmail.com>
2023-06-30 11:34:49 +0530

    first commit

---
+++ src/test/java/demoJenkins/UsingPOM/AppTest.java
@@ -0,0 +1,15 @@
+package demoJenkins.UsingPOM;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+	  
+	  System.out.println("This is just an demo of project");
+    assertTrue(true);
+  }
+}
